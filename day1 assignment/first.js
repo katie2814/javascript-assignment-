23c33dfd5a89988ae4b841612b2a87eee72e7e07
Aarthy <59475015+katie2814@users.noreply.github.com>
2020-07-17 11:22:06 +0530

    Add files via upload

---
+++ day1 assignment/first.js
@@ -0,0 +1,53 @@
+//QUESTION 1
+
+//log prints the message in the console
+console.log("Hi this is Aarthy")
+//warn gives a warning
+console.warn("warning")
+//error gives an error
+console.error("error take care")
+
+
+//QUESTION 2
+
+//var-var is like a global variable
+var a;
+a="aarthy";
+console.log(a)
+
+//let-let is like a local variable and it is widely used for efficiency.
+{
+let a;
+a="anwesha";
+console.log(a)
+}
+//const-fixed and value should be assigned when declared
+
+const c="katie";
+console.log(c)
+
+//QUESTION 3
+
+//Datatypes in Javascript
+/* Javascript has two dataypes:
+ Primitive and Reference
+ in Primitive      in Reference
+ *Number             *Array
+ *String             *Functions
+ *Boolean            *Objects
+ *BigInt
+ *null
+ *Undefined
+*/
+let h=123;
+console.log(h);
+let i="qwinsa";
+console.log(i);
+let j=true;
+console.log(j);
+let k=12345n;
+console.log(k);
+let l=null;
+console.log(l);
+let m;
+console.log(m);
