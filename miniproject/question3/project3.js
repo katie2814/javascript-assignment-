f717682039623e5bcd54be7d5190d37fd6c15950
Aarthy <59475015+katie2814@users.noreply.github.com>
2020-08-02 19:15:25 +0530

    miniproject

---
+++ miniproject/question3/project3.js
@@ -0,0 +1,10 @@
+var quotes=['\"I have always believed that each man makes his own happiness and is responsible for his own problems. It is a simple philosophy.\"-Ray Kroc',
+'\"When we have respect for ourselves and others, we gravitate towards connections that encourage that.\"-Simeon Lindstrom, Codependency','\"Anger is the ultimate destroyer of your own peace of mind.\"-Dalai Lama','\"A man should have the aim and the determination to be honest and upright and sincere in all that he undertakes. If he adds persistency to this he can hardly help being successful\"- L. R. Ellert',
+'\"Only one thing is ever guaranteed, that is that you will definitely not achieve the goal if you don\'t take the shot.\"- Wayne Gretzky','\"Don\'t be afraid. Be focused. Be determined. Be hopeful. Be empowered.\"- Michelle Obama','\"The fact is that grief today is a family matter as much a s it is an individual one.\"- Barbara Okun','\"Children really brighten up a household. They never turn the lights off.\"- Ralph Bus','\"No one would have crossed the ocean if he could have gotten off the ship in the storm.\"-Charles Kettering','\"Congratulations! today is your day. You\'re off to Great Places! You\'re off and away.\"- Dr. Seuss','\"Appreciate those early influences and what they\'ve done for you.\"-Willie Davis','\"Emotional empathy is what motivates us to help others.\"-Brian Goldman, The Power of Kindness',
+'\"The true wealth of a nation lies not in it\'s gold or silver but in it\'s learning, wisdom and in the uprightness of its sons.\"- Kahlil Gibran',
+'\"Make the decision, make another. Remake one past, you cannot.\"- Yoda','\"Be honest in your feelings, for they are the surest conduit to knowledge...\"- Anoon Bondara - Star War Quotes'];
+function quote()
+{
+    var a=Math.floor(Math.random()*(quotes.length));
+    document.getElementById('quote').innerHTML=quotes[a];
+}
